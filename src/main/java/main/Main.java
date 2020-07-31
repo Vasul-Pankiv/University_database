e6516954795d4c95e2b0151c4c157562d7bf4e3b
vasul <pannkiv.v@gmail.com>
2020-07-31 12:49:08 +0300

    optimized code and fixed naming

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,6 @@
 package main;
 
-import model.Degree;
-import model.Department;
 import model.Lector;
-import services.DegreeService;
 import services.DepartmentService;
 import services.LectorService;
 
@@ -16,12 +13,11 @@ public class Main {
     public static void main(String[] args) throws SQLException, IOException {
         DepartmentService departmentService = new DepartmentService();
         LectorService lectorService = new LectorService();
-        DegreeService degreeService = new DegreeService();
-        String message, department_name,template;
+        String message, departmentName, template;
         Scanner sc = new Scanner(System.in);
         String command = "";
 
-        while (!command.equals("0")){
+        while (!command.equals("0")) {
 
             System.out.println("Menu:\n" +
                     "1. Who is head of department {department_name}\n" +
@@ -29,59 +25,54 @@ public class Main {
                     "3. Show the average salary for department {department name}\n" +
                     "4. Show count of employee for department {department_name}\n" +
                     "5. Global search by {template}\n" +
-                    "0. exit\n"+
+                    "0. exit\n" +
                     "Your choice: ");
             command = sc.next();
 
-            switch (command){
+            switch (command) {
                 case "1":
                     System.out.println("Input department name: ");
-                    department_name = sc.next();
-                    if(departmentService.findByName(department_name)!=null){
-                        if(departmentService.getHeadByDepartmentName(department_name)==null){
+                    departmentName = sc.next();
+                    if (departmentService.findByName(departmentName) != null) {
+                        if (departmentService.getHeadByDepartmentName(departmentName) == null) {
                             System.out.println("Head does not exist!");
-                        }
-                        else{
-                            String head_name = departmentService.getHeadByDepartmentName(department_name).getName();
-                            message = "Haed of "+department_name+" department is: "+head_name;
+                        } else {
+                            String head_name = departmentService.getHeadByDepartmentName(departmentName).getName();
+                            message = "Haed of " + departmentName + " department is: " + head_name;
                             System.out.println(message);
                         }
-                    }
-                    else {
+                    } else {
                         System.out.println("Department does not exist!");
                     }
                     break;
                 case "2":
                     System.out.println("Input department name: ");
-                    department_name = sc.next();
-                    if(departmentService.findByName(department_name)!=null){
-                        message = departmentService.getDepartmentStatisticByDepartmentName(department_name);
+                    departmentName = sc.next();
+                    if (departmentService.findByName(departmentName) != null) {
+                        message = departmentService.getDepartmentStatisticByDepartmentName(departmentName);
                         System.out.println(message);
-                    }
-                    else{
+                    } else {
                         System.out.println("Department does not exist!");
                     }
 
                     break;
                 case "3":
                     System.out.println("Input department name: ");
-                    department_name = sc.next();
-                    if(departmentService.findByName(department_name)!=null){
-                        message = "Average salary of "+department_name+"department: "+departmentService.getAverageSalaryByDepartmentName(department_name);
+                    departmentName = sc.next();
+                    if (departmentService.findByName(departmentName) != null) {
+                        message = "Average salary of " + departmentName + "department: " + departmentService.getAverageSalaryByDepartmentName(departmentName);
                         System.out.println(message);
-                    }
-                    else{
+                    } else {
                         System.out.println("Department does not exist!");
                     }
                     break;
                 case "4":
                     System.out.println("Input department name: ");
-                    department_name = sc.next();
-                    if(departmentService.findByName(department_name)!=null){
-                        message = department_name+" employees count: "+departmentService.getDepartmentEmployeesCount(department_name);
+                    departmentName = sc.next();
+                    if (departmentService.findByName(departmentName) != null) {
+                        message = departmentName + " employees count: " + departmentService.getDepartmentEmployeesCount(departmentName);
                         System.out.println(message);
-                    }
-                    else{
+                    } else {
                         System.out.println("Department does not exist!");
                     }
                     break;
@@ -89,12 +80,11 @@ public class Main {
                     System.out.println("Input template: ");
                     template = sc.next();
                     Set<Lector> lectors = lectorService.globalSearch(template);
-                    if(lectors.isEmpty()){
+                    if (lectors.isEmpty()) {
                         System.out.println("Matches not found...");
-                    }
-                    else{
+                    } else {
                         for (Lector l : lectors) {
-                            System.out.println("Name: "+l.getName()+" second name: "+l.getSecond_name());
+                            System.out.println("Name: " + l.getName() + " second name: " + l.getSecond_name());
                         }
                     }
                     break;
